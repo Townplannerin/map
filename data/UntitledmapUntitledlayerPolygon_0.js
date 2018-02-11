6d42173348e48e5574c66b03e5cf94e3729a9e79
Townplannerin <34913596+Townplannerin@users.noreply.github.com>
2018-02-11 23:24:30 +0530

    Add files via upload

---
+++ data/UntitledmapUntitledlayerPolygon_0.js
@@ -0,0 +1,8 @@
+var json_UntitledmapUntitledlayerPolygon_0 = {
+"type": "FeatureCollection",
+"name": "UntitledmapUntitledlayerPolygon_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "MS BUILDING\n", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": 1.0, "extrude": 0.0, "visibility": 1.0, "drawOrder": null, "icon": null }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 77.587945, 12.9782615, 0.0 ], [ 77.5881609, 12.9778524, 0.0 ], [ 77.5883259, 12.9779348, 0.0 ], [ 77.5881086, 12.9783464, 0.0 ], [ 77.587945, 12.9782615, 0.0 ] ] ] } }
+]
+}
