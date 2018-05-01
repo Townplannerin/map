7bb12157113c63830488ec557ebf985dc6b8c0a8
Townplannerin <34913596+Townplannerin@users.noreply.github.com>
2018-05-01 19:16:03 +0530

    Delete SribayaluGanaptiTemple_0.js

--- data/SribayaluGanaptiTemple_0.js
+++
@@ -1,8 +0,0 @@
-var json_SribayaluGanaptiTemple_0 = {
-"type": "FeatureCollection",
-"name": "SribayaluGanaptiTemple_0",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "File": "Sri bayalu Ganapti Temple" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 76.184597280867422, 14.04737149490234 ], [ 76.184644439445592, 14.047375014005222 ], [ 76.18464988081999, 14.047330145439609 ], [ 76.184597280867422, 14.047327506111953 ], [ 76.184597280867422, 14.04737149490234 ] ] ] } }
-]
-}
