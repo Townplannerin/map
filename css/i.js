6e6de9f1a8098f124df37960096fdea2ae27647e
Townplannerin <34913596+Townplannerin@users.noreply.github.com>
2019-03-13 12:36:02 +0530

    Add files via upload

---
+++ css/i.js
@@ -0,0 +1,67 @@
+﻿var highlightLayer;
+        function highlightFeature(e) {
+            highlightLayer = e.target;
+
+            if (e.target.feature.geometry.type === 'LineString') {
+              highlightLayer.setStyle({
+                color: '#ffff00',
+              });
+            } else {
+              highlightLayer.setStyle({
+                fillColor: '#ffff00',
+                fillOpacity: 1
+              });
+            }
+            highlightLayer.openPopup();
+        }
+        var map = L.map('map', {
+            zoomControl:true, maxZoom:21, minZoom:1
+        }).fitBounds([[14.0471419861,76.1843008714],[14.0475410523,76.185065252]]);
+        var hash = new L.Hash(map);
+        map.attributionControl.addAttribution('<a href="" target="_blank">WEB</a>');
+       
+        var bounds_group = new L.featureGroup([]);
+        var GoogleStreet =
+          L.tileLayer('http://www.google.com/maps/vt?lyrs=m@189&gl=com&x={x}&y={y}&z={z}', {
+            attribution: 'Imagery © <a href="http://maps.google.com">Google</a>',
+            maxZoom: 21
+          })
+        var GoogleSatellite =
+          L.tileLayer('http://www.google.com/maps/vt?lyrs=s@189&gl=com&x={x}&y={y}&z={z}', {
+            attribution: 'Imagery © <a href="http://maps.google.com">Google</a>',
+            maxZoom: 21
+          })
+        var GoogleHybrid =
+          L.tileLayer('http://www.google.com/maps/vt?lyrs=s,h@189&gl=com&x={x}&y={y}&z={z}', {
+            attribution: 'Imagery © <a href="http://maps.google.com">Google</a>',
+            maxZoom: 21
+          })
+        map.addLayer(GoogleSatellite);
+        var Bengaluru =
+          L.tileLayer('data/BASE MAP Nayakanahatti/{z}/{x}/{y}.png', {
+            opacity: 0.30,
+            transparent: true,
+	    tms: false,
+            attribution: 'Imagery © <a href=" ">A</a>',
+            maxZoom: 16
+          }).addTo(map)
+        function popUp(feature, layer) {
+    	layer.bindPopup(feature.properties.name);
+  	}
+
+  	var geojsonLayer = new L.GeoJSON.AJAX("data/SriBayaluGanaptiTemple.geojson", {onEachFeature:popUp});
+        geojsonLayer.addTo(map);
+
+	function onMapClick(e) {
+		c.setCoordinates(e);
+	}
+	map.on('click', onMapClick);
+        var baseMaps = {'GoogleStreet': GoogleStreet,
+            'GoogleSatellite': GoogleSatellite,
+            'GoogleHybrid': GoogleHybrid
+        };
+        var overlayLayers = {
+	        'Village': Bengaluru,
+	        '<img src="legend/SribayaluGanaptiTemple_0.png" /> Sri bayalu Ganapti Temple': geojsonLayer
+        }
+        L.control.layers(baseMaps, overlayLayers,{collapsed:true}).addTo(map);
