70f990d822f011a123c12b1e15902c0ff384ca38
Townplannerin <34913596+Townplannerin@users.noreply.github.com>
2022-11-11 18:10:33 +0530

    Update i.js

--- css/i.js
+++ css/i.js
@@ -61,7 +61,6 @@
             'GoogleHybrid': GoogleHybrid
         };
         var overlayLayers = {
-	        'Village': Bengaluru,
 	        '<img src="legend/SribayaluGanaptiTemple_0.png" /> Sri bayalu Ganapti Temple': geojsonLayer
         }
         L.control.layers(baseMaps, overlayLayers,{collapsed:true}).addTo(map);
